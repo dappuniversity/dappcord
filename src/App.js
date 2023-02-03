6d2372057fde7617c2d26fe9c2717e10f1cbdd0c
Justin <justin7359900@gmail.com>
2023-02-03 11:02:30 -0500

    Adds starter code

--- src/App.js
+++ src/App.js
@@ -18,79 +18,12 @@ import config from './config.json';
 const socket = io('ws://localhost:3030');
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
-
-  const [dappcord, setDappcord] = useState(null)
-  const [channels, setChannels] = useState([])
-
-  const [currentChannel, setCurrentChannel] = useState(null)
-  const [messages, setMessages] = useState([])
-
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-
-    const network = await provider.getNetwork()
-    const dappcord = new ethers.Contract(config[network.chainId].Dappcord.address, Dappcord, provider)
-    setDappcord(dappcord)
-
-    const totalChannels = await dappcord.totalChannels()
-    const channels = []
-
-    for (var i = 1; i <= totalChannels; i++) {
-      const channel = await dappcord.getChannel(i)
-      channels.push(channel)
-    }
-
-    setChannels(channels)
-
-    window.ethereum.on('accountsChanged', async () => {
-      window.location.reload()
-    })
-  }
-
-  useEffect(() => {
-    loadBlockchainData()
-
-    // --> https://socket.io/how-to/use-with-react-hooks
-
-    socket.on("connect", () => {
-      socket.emit('get messages')
-    })
-
-    socket.on('new message', (messages) => {
-      setMessages(messages)
-    })
-
-    socket.on('get messages', (messages) => {
-      setMessages(messages)
-    })
-
-    return () => {
-      socket.off('connect')
-      socket.off('new message')
-      socket.off('get messages')
-    }
-  }, [])
 
   return (
     <div>
-      <Navigation account={account} setAccount={setAccount} />
 
       <main>
-        <Servers />
-
-        <Channels
-          provider={provider}
-          account={account}
-          dappcord={dappcord}
-          channels={channels}
-          currentChannel={currentChannel}
-          setCurrentChannel={setCurrentChannel}
-        />
-
-        <Messages account={account} messages={messages} currentChannel={currentChannel} />
+        <h1>Welcome to Dappcord</h1>
       </main>
     </div>
   );
