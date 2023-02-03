d4993147919f68bb93b063fe82ddb57ed8853978
Justin <justin7359900@gmail.com>
2023-02-03 11:05:41 -0500

    Modified starter message

--- src/App.js
+++ src/App.js
@@ -21,9 +21,10 @@ function App() {
 
   return (
     <div>
+      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
 
       <main>
-        <h1>Welcome to Dappcord</h1>
+
       </main>
     </div>
   );
