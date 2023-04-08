0a6fd8e6b75f5bdcf4fac6ebb664c1071733aa18
Arefin <handsonweb3@gmail.com>
2023-04-08 19:18:34 +0600

    first commit

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Dappcord {}
+contract Dappacord is ERC721 {
+    constructor(  
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
+}
